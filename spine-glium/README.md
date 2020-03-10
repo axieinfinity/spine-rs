9765d23aef405dcd9a18adfbf509138bc5350e51
Trung Nguyen <me@trungfinity.com>
2020-03-10 19:42:11 +0700

    Remove spine-glium crate

--- spine-glium/README.md
+++
@@ -1,3 +0,0 @@
-# spine-glium
-
-Spine runtime for Rust with Glium backend.
--- spine/examples/glium.rs
+++ spine/examples/glium.rs
@@ -2,7 +2,12 @@ use std::error::Error;
 
 use glium::Surface;
 use image::{DynamicImage, GenericImageView};
-use spine::Renderer;
+use spine::{
+    animation::{AnimationState, AnimationStateData},
+    atlas::Atlas,
+    skeleton::{Skeleton, SkeletonData, SkeletonJson},
+    Renderer,
+};
 
 #[repr(transparent)]
 struct Texture(DynamicImage);
@@ -77,4 +82,21 @@ impl Renderer for Display {
     fn finish(&self, frame: Self::Frame) -> spine::Result<()> {
         frame.finish().map_err(spine::Error::render)
     }
+}
+
+fn main() -> spine::Result<()> {
+    let atlas = Atlas::from_file("/Users/trung/Downloads/normal.atlas")?;
+
+    let mut skeleton_json = SkeletonJson::from_atlas(&atlas);
+    skeleton_json.set_scale(0.5);
+
+    let skeleton_data =
+        SkeletonData::from_json_file("/Users/trung/Downloads/normal.json", &skeleton_json)?;
+
+    let animation_state_data = AnimationStateData::new(&skeleton_data);
+
+    let _skeleton = Skeleton::new(&skeleton_data);
+    let _animation_state = AnimationState::new(&animation_state_data);
+
+    Ok(())
 }
