f2edfd31955796ca92948992e9f8e6cb4267b92b
Trung Nguyen <me@trungfinity.com>
2020-03-11 07:10:04 +0700

    Support controlling animations via animation state type

--- spine/src/animation/state.rs
+++ spine/src/animation/state.rs
@@ -1,6 +1,11 @@
-use std::{marker::PhantomData, ptr::NonNull};
+use std::{ffi::CString, marker::PhantomData, os::raw::c_int, ptr::NonNull};
 
-use spine_sys::{spAnimationState, spAnimationState_create, spAnimationState_dispose};
+use spine_sys::{
+    spAnimationState, spAnimationState_apply, spAnimationState_create, spAnimationState_dispose,
+    spAnimationState_setAnimationByName, spAnimationState_update,
+};
+
+use crate::{result::Result, skeleton::Skeleton};
 
 use super::state_data::AnimationStateData;
 
@@ -15,6 +20,34 @@ impl<'a> AnimationState<'a> {
         let pointer = unsafe { spAnimationState_create(animation_state_data.0.as_ptr()) };
         let pointer = NonNull::new(pointer).unwrap();
         AnimationState(pointer, PhantomData)
+    }
+
+    pub fn set_animation_by_name(
+        &mut self,
+        track_index: usize,
+        animation_name: &str,
+        r#loop: bool,
+    ) -> Result<()> {
+        let animation_name = CString::new(animation_name)?;
+
+        let _track_entry = unsafe {
+            spAnimationState_setAnimationByName(
+                self.0.as_ptr(),
+                track_index as c_int,
+                animation_name.as_ptr(),
+                r#loop as c_int,
+            )
+        };
+
+        Ok(())
+    }
+
+    pub fn update(&mut self, delta: f32) {
+        unsafe { spAnimationState_update(self.0.as_ptr(), delta) }
+    }
+
+    pub fn apply(&mut self, skeleton: &mut Skeleton) {
+        let _result = unsafe { spAnimationState_apply(self.0.as_ptr(), skeleton.0.as_ptr()) };
     }
 }
 
